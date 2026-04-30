9aedb6d5cf18b4a7014e1daec1e507c2879bd5da
James McDonald jhmcdo3 <jhmcdo3@uwe.nsa.gov>
2026-04-30 12:27:12 -0400

    updated routing

--- local_flix/src/app/app-routing-module.ts
+++ local_flix/src/app/app-routing-module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home';
 import { MovieDetail } from './features/movie-detail/movie-detail';
+import { ImageGridComponent } from './features/image-grid/image-grid';
+import { MovieGridComponent } from './features/movie-grid/movie-grid';
+import { ComicGridComponent } from './features/comic-grid/comic-grid';
+import { MusicGridComponent } from './features/music-grid/music-grid';
 import {ComicDetail} from './features/comic-detail/comic-detail'
 import { ImageDetail } from './features/image-detail/image-detail';
 import { MusicDetail } from './features/music-detail/music-detail';
@@ -12,7 +16,14 @@ import { AppComponent } from './app';
 
 const routes: Routes = [
   { path: 'home/', component: HomeComponent },
-  { path: 'movie/:id', component: MovieDetail },
+  { path: 'movie/', component: MovieGridComponent },
+  { path: 'comic/', component: ComicGridComponent},
+  { path: 'music/', component: MusicGridComponent},
+  { path: 'image/', component: ImageGridComponent},
+  { path: 'movie_detail/:id', component: MovieDetail},
+  { path: 'comic_detail/:id', component: ComicDetail},
+  { path: 'image_detail/:id', component: ImageDetail},
+  { path: 'music_detail/:id', component: MusicDetail},
   { path: 'player/:id', component: Player }
 ];
 
